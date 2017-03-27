fa1124408022fbad6de749dd7aa376099b4ca18d
Naoki_Sawada <appdev1407@gmail.com>
2017-03-27 22:07:48 +0900

    内部処理メモ

--- test01/src/app/app.module.ts
+++ test01/src/app/app.module.ts
@@ -1,3 +1,5 @@
+//アプリ全体の設定情報記述
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -6,14 +8,20 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
+
+  // このアプリで利用する
   declarations: [
     AppComponent
   ],
+
+  // このアプリで利用するモジュール
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+
+  // DI元となるクラス
   providers: [],
   bootstrap: [AppComponent]
 })
