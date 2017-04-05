6a3a10b05e56737973575a3ff8931a4022ef205f
Naoki_Sawada <appdev1407@gmail.com>
2017-04-05 20:47:23 +0900

    add sample

--- test01/src/app/app.module.ts
+++ test01/src/app/app.module.ts
@@ -4,25 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MainComponent, ROUTES } from './main.component';
+import { Page01Component } from './page01.component';
+import { Page02Component } from './page02.component';
+import { CalcService } from './calc.service';
+import { StoreService } from './store.service';
+import { ChildComponent } from './child.component';
+import { CounterService } from './counter.service';
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 
 @NgModule({
 
   // このアプリで利用する
   declarations: [
-    AppComponent
+    MainComponent,
+    Page01Component,
+    Page02Component,
+    ChildComponent
   ],
 
   // このアプリで利用するモジュール
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
 
   // DI元となるクラス
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    CalcService,
+    StoreService,
+    CounterService
+  ],
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
